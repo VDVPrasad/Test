3e51effb703dcac49b942f440249bfb82524a1b8
VDVPrasad <57435554+VDVPrasad@users.noreply.github.com>
2019-11-06 16:17:10 +0530

    Update OrederUpdatePUT.java

--- OrederUpdatePUT.java
+++ OrederUpdatePUT.java
@@ -1,9 +1,50 @@
-URL url = new URL("http://www.example.com/resource");
-HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
-httpCon.setDoOutput(true);
-httpCon.setRequestMethod("PUT");
-OutputStreamWriter out = new OutputStreamWriter(
-    httpCon.getOutputStream());
-out.write("Resource content");
-out.close();
-httpCon.getInputStream();
+
+
+
+public class OrderClientPost {
+
+	// http://localhost:8080/FictionalOrganization/bricks/customer/post
+	public static void main(String[] args) {
+
+	  try {
+
+        URL url = new URL("http://localhost:8080/FictionalOrganization/bricks/customer/put");
+        HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+        httpCon.setDoOutput(true);
+        httpCon.setRequestMethod("PUT");
+          String input = "{\"name\":Kiran,\"OrderId\":\"00001, \"numberOfBricks\"100000 \"}";
+        OutputStreamWriter out = new OutputStreamWriter(
+        httpCon.getOutputStream());
+        out.write(input);
+        out.close();
+        httpCon.getInputStream();
+          
+			if (conn.getResponseCode() != HttpURLConnection.HTTP_UPDATED) {
+			throw new RuntimeException("Failed : HTTP error code : "
+				+ conn.getResponseCode());
+		}
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				(conn.getInputStream())));
+
+		String output;
+		System.out.println("No of bricks are updated : \n");
+		while ((output = br.readLine()) != null) {
+			System.out.println(output);
+		}
+
+		conn.disconnect();
+
+	  } catch (MalformedURLException e) {
+
+		e.printStackTrace();
+
+	  } catch (IOException e) {
+
+		e.printStackTrace();
+
+	 }
+
+	}
+
+}
